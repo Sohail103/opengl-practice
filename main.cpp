aa5747084379884bf1c0ab471855e4faabae467c
Sohail103 <sohailraj.satapathy@gmail.com>
2025-08-16 12:34:08 +0530

    creating a window

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwInit();                                    // initializing glfw
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // telling opengl the version of opengl we are using
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // choosing between core or
+                                                                   // compatibility profile
+
+    GLFWwindow *window = glfwCreateWindow(800, 800, "window 1", NULL, NULL); // glfwCreate window takes
+                                                                             // parameters width, height, name, fullscreen or not and one more parameter
 
     glfwTerminate();
     return 0;
